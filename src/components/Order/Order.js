b46ed1f96e699ea204202bce35b09014124fda44
MdShohanAli <720shohan@gmail.com>
2022-11-05 20:21:42 +0600

    complete-cart

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import useCart from '../../useProduct/useCart';
 import useProducts from '../../useProduct/useProducts';
+import { removeFromDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import ReviewItem from '../ReviewItem/ReviewItem';
+import { useNavigate } from 'react-router-dom';
+import './Order.css'
 
 const Order = () => {
     const [products, setProducts] = useProducts();
     const [cart, setCart] = useCart(products);
-
+    const navigate = useNavigate();
     const handelRemoveContainer = product => {
         const rest = cart.filter(pd => pd.id !== product.id)
         setCart(rest)
+        removeFromDb(product.id);
     }
     return (
         <div>
@@ -25,7 +29,11 @@ const Order = () => {
                     }
                 </div>
                 <div className="cart-container">
-                    <Cart cart={cart} ></Cart>
+                    <Cart cart={cart}>
+
+                        <button onClick={() => navigate('/inventory')} >Process To Checkout</button>
+
+                    </Cart>
                 </div>
 
             </div>
